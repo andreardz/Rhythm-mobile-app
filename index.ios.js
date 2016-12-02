48b9bb5d06ef54d52307b3cfc7a08764f9e57a1a
Michael Freeman <Michael@rescomp-16-288208.stanford.edu>
2016-12-01 23:44:37 -0800

    Sign in screen 2

--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,52 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableHighlight,
+  Alert,
+  Button,
 } from 'react-native';
 
+
+const onButtonPress = () => {
+  Alert.alert('Button has been pressed!');
+};
+
+
 export default class Rhythm extends Component {
+
+  onPressed() {
+    return (
+      <Image
+          style={{width: 280, height: 51, top: 25, borderRadius: 10}}
+          source={require('./img/loginwithfb2.png')}/>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
+
         <Text style={styles.Rhythm}>
           RHYTHM
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
+        <Image
+          style={{width: 280, height: 51, top: 180, borderRadius: 10}}
+          source={require('./img/loginwithfb2.png')}/>
+
+        <View style={styles.signUpBox}>
+          <Button onPress={this.onPressed} style={styles.signUp} title="Sign Up">
+          </Button>
+        </View>
+
+        <View style={styles.loginBox}>
+          <Text style={styles.signUp}>
+            Login with E-mail
+          </Text>
+        </View>
+
       </View>
     );
   }
@@ -34,22 +63,40 @@ export default class Rhythm extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#9B51E0',
+  },
+  signUpBox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 205,
+    height: 51,
+    width: 280,
+    backgroundColor: "#743BA8",
+    borderRadius: 10,
+  },
+  loginBox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 230,
+    height: 51,
+    width: 280,
+    backgroundColor: "#AF7EDD",
+    borderRadius: 10,
   },
   Rhythm: {
     color: '#FFFFFF',
     fontFamily: 'Blanch',
     fontSize: 123,
-    top: 169,
+    top: 150,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  signUp: {
+    color: '#FFFFFF',
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    fontSize: 20,
+    fontFamily: 'SanFranciscoDisplay-Bold',
   },
 });
 
